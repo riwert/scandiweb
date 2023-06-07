68fb222607949fdc75d82e17b897074c7a892aa9
Riwert <riwert@gmail.com>
2023-06-08 01:30:46 +0200

    remove cm from dimensions to match spec

--- client/components/ProductFeatures.vue
+++ client/components/ProductFeatures.vue
@@ -12,7 +12,7 @@ const props = defineProps({
     <p class="block py-2 font-bold typography-text-lg">${{ product.price }}</p>
     <p class="block py-2 font-normal leading-5 typography-text-sm text-neutral-700" v-if="product.productType == 'dvd'">Size: {{ product.size }} MB</p>
     <p class="block py-2 font-normal leading-5 typography-text-sm text-neutral-700" v-if="product.productType == 'book'">Weight: {{ product.weight }} kg</p>
-    <p class="block py-2 font-normal leading-5 typography-text-sm text-neutral-700" v-if="product.productType == 'furniture'">Dimensions: {{ product.height }}x{{ product.width }}x{{ product.length }} cm</p>
+    <p class="block py-2 font-normal leading-5 typography-text-sm text-neutral-700" v-if="product.productType == 'furniture'">Dimensions: {{ product.height }}x{{ product.width }}x{{ product.length }}</p>
 
   </div>
 </template>
