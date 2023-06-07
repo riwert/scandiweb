88214c3a37019ba587e4f3018aae359651b50ad6
Riwert <riwert@gmail.com>
2023-06-07 16:35:24 +0200

    add cors setup to api response

--- api/config/Response.php
+++ api/config/Response.php
@@ -4,10 +4,21 @@ namespace RAPI\config;
 
 class Response
 {
-    public static function handle($json, $code)
+    public static function handle($json, $code = 500)
     {
+        // Set CORS headers
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+        // Handle preflight OPTIONS request
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(200);
+            exit();
+        }
+
         header('Content-Type: application/json');
-        http_response_code($code || 500);
+        http_response_code($code);
         echo json_encode($json);
         exit();
     }
