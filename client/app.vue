42fb64379ce337cc23d6df01440562a6565cdc0d
Riwert <riwert@gmail.com>
2023-06-09 18:26:29 +0200

    add meta & pwa

--- client/app.vue
+++ client/app.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+useServerSeoMeta({
+  charset: 'utf-8',
+  viewport: 'width=device-width, initial-scale=1',
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  title: 'Scandiweb Products',
+  meta: [
+    { name: 'description', content: 'Mid-senior & Senior Web Developer test assignment in Scandiweb' },
+    { name: 'author', content: 'revert@revert.pl' },
+    { name: 'theme-color', content: '#dd4746' },
+  ],
+  link: [
+    { rel: 'manifest', href: '/manifest.json' },
+    { rel: 'apple-touch-icon', href: '/favicon/apple-touch-icon.png', sizes: '180x180' },
+    { rel: 'icon', href: '/favicon/favicon.ico' },
+    { rel: 'icon', href: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    { rel: 'icon', href: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+  ],
+  noscript: [
+    {
+      children: 'JavaScript is required.',
+      body: true
+    }
+  ],
+})
+</script>
+
 <template>
   <div class="app">
     <NuxtLoadingIndicator />
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,17 +2,39 @@
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
+  runtimeConfig: {
+    NODE_ENV: process.env.NODE_ENV,
+    public: {
+      NUXT_API_URL: process.env.NUXT_API_URL || '/api',
+    },
+  },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
   css: [
     '~/assets/scss/styles.scss',
   ],
-  modules: ['@nuxtjs/tailwindcss'],
-  runtimeConfig: {
-    NODE_ENV: process.env.NODE_ENV,
-    public: {
-      NUXT_API_URL: process.env.NUXT_API_URL || '/api',
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vite-pwa/nuxt'
+  ],
+  pwa: {
+    registerType: 'autoUpdate',
+    icon: false,
+    meta: false,
+    manifest: false,
+    strategies: 'generateSW',
+    workbox: {
+      globPatterns: ['**/*.{js,css,png,svg,ico}'],
+      navigateFallback: null,
     },
+  },
+  vite: {
+    server: {
+      hmr: {
+        protocol: 'ws',
+        host: 'localhost'
+      }
+    }
   },
 })
