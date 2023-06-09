35d05cf53077ecaf79a601cce813e5a0369021fc
Riwert <riwert@gmail.com>
2023-06-09 14:33:04 +0200

    add env vars read

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -8,4 +8,10 @@ export default defineNuxtConfig({
     '~/assets/scss/styles.scss',
   ],
   modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    NODE_ENV: process.env.NODE_ENV,
+    public: {
+      NUXT_API_URL: process.env.NUXT_API_URL || '/api',
+    },
+  },
 })
