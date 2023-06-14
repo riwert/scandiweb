6a129fb151ff3ed279632cd2de02c44e0b6df564
Riwert <riwert@gmail.com>
2023-06-14 16:51:30 +0200

    add more conditions to product details

--- client/components/ProductDetails.vue
+++ client/components/ProductDetails.vue
@@ -7,12 +7,12 @@ const props = defineProps({
 <template>
   <div class="product__features">
 
-    <p class="block py-2 font-normal typography-text-sm text-neutral-500">{{ product.sku }}</p>
-    <p class="block py-2 font-medium typography-text-lg text-neutral-700">{{ product.name }}</p>
-    <p class="block py-2 font-bold typography-text-lg text-neutral-700">${{ product.price }}</p>
-    <p class="block py-2 font-normal typography-text-sm text-neutral-500" v-if="product.productType == 'dvd'">Size: {{ useTrimZeros(product.size) }} MB</p>
-    <p class="block py-2 font-normal typography-text-sm text-neutral-500" v-if="product.productType == 'book'">Weight: {{ useTrimZeros(product.weight) }} kg</p>
-    <p class="block py-2 font-normal typography-text-sm text-neutral-500" v-if="product.productType == 'furniture'">Dimensions: {{ useTrimZeros(product.height) }}x{{ useTrimZeros(product.width) }}x{{ useTrimZeros(product.length) }} cm</p>
+    <p class="block py-2 font-normal typography-text-sm text-neutral-500" v-if="product.sku">{{ product.sku }}</p>
+    <p class="block py-2 font-medium typography-text-lg text-neutral-700" v-if="product.name">{{ product.name }}</p>
+    <p class="block py-2 font-bold typography-text-lg text-neutral-700" v-if="product.price">${{ useTrimZeros(product.price) }}</p>
+    <p class="block py-2 font-normal typography-text-sm text-neutral-500" v-if="product.size && product.productType == 'dvd'">Size: {{ useTrimZeros(product.size) }} MB</p>
+    <p class="block py-2 font-normal typography-text-sm text-neutral-500" v-if="product.weight && product.productType == 'book'">Weight: {{ useTrimZeros(product.weight) }} kg</p>
+    <p class="block py-2 font-normal typography-text-sm text-neutral-500" v-if="product.height && product.productType == 'furniture'">Dimensions: {{ useTrimZeros(product.height) }}x{{ useTrimZeros(product.width) }}x{{ useTrimZeros(product.length) }} cm</p>
 
   </div>
 </template>
