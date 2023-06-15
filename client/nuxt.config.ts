2eaf5e89936c21b4cf4cb8b17bd2df9ddd2eaa8c
Riwert <riwert@gmail.com>
2023-06-15 17:04:45 +0200

    change render to full ssr mode

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // ssr: false,
+  target: 'server',
+  ssr: true,
   // experimental: {
   //   payloadExtraction: false,
   // },
