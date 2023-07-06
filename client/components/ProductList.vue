a7a7a8e24901a0c4edce31035e6bc4c7ba92220e
Riwert <riwert@gmail.com>
2023-07-06 16:20:06 +0200

    change selector class name for getting checkboxes

--- client/components/ProductList.vue
+++ client/components/ProductList.vue
@@ -49,7 +49,7 @@ const resetMessages = () => {
 
 // fallback for testing when programmatically checked checkboxes
 const getCheckedCheckboxes = () => {
-  const checkboxes = document.getElementsByClassName('delete-checkbox')
+  const checkboxes = document.getElementsByClassName('checkbox')
 
   let checkedCheckboxes = []
   for (let i=0; i<checkboxes?.length; i++) {
