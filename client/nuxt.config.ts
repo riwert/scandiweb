3e13ef752b01e774a83aed247d84212cf7611062
Riwert <riwert@gmail.com>
2023-07-07 14:14:56 +0200

    set ssr true again

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   // ssr: true,
   routeRules: {
-    '/**': { isr: false },
+    '/**': { ssr: true },
     // SSR Redirect
     // '/products': { redirect: '/product/list' }
   },
