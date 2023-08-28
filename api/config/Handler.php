aef0c4d061ee8b501437251e2837fca8b51a5fda
Riwert <riwert@gmail.com>
2023-08-28 14:23:32 +0200

    fix hander class formatting

--- api/config/Handler.php
+++ api/config/Handler.php
@@ -11,27 +11,27 @@ class Handler
     {
         // Create an array with the exception details
         $error = [
-          'type' => 'exception',
-          'code' => $exception->getCode(),
-          'error' => $exception->getMessage(),
-          'file' => $exception->getFile(),
-          'line' => $exception->getLine()
-      ];
-      // Return the JSON response with appropriate HTTP status code
-      return Response::handle($error, 500);
+            'type' => 'exception',
+            'code' => $exception->getCode(),
+            'error' => $exception->getMessage(),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine()
+        ];
+        // Return the JSON response with appropriate HTTP status code
+        return Response::handle($error, 500);
     }
 
     public static function handleError($errno, $errstr, $errfile, $errline): string
     {
         // Create an array with the error details
         $error = [
-          'type' => 'error',
-          'code' => $errno,
-          'error' => $errstr,
-          'file' => $errfile,
-          'line' => $errline
-      ];
-      // Return the JSON response with appropriate HTTP status code
-      return Response::handle($error, 500);
+            'type' => 'error',
+            'code' => $errno,
+            'error' => $errstr,
+            'file' => $errfile,
+            'line' => $errline
+        ];
+        // Return the JSON response with appropriate HTTP status code
+        return Response::handle($error, 500);
     }
 }
